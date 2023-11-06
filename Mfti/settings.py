fc0fe521ef77dff0f952b3d0779d4e025cb440d5
NikeSibirRus <nikesibir@yandex.ru>
2023-11-06 19:35:18 +0700

    второй урок2

--- Mfti/settings.py
+++ Mfti/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
@@ -51,11 +53,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Mfti.urls'
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views.py
+++ main/views.py
@@ -2,13 +2,17 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
 
 
 def index(request):
     return render(request,'main/index.html')
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about.html', { "form" : NameForm() })
 
 def contacts(request):
     return render(request, 'main/contacts.html')
